64456d451dc6ab3fb9c6a948e13b38e27324c322
Aditya Deshmukh <64344005+Adity-Deshmukh@users.noreply.github.com>
2020-06-06 16:28:39 +0530

    changed the architecture

--- discriminator.py
+++ discriminator.py
@@ -6,7 +6,6 @@ from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormaliza
 from keras.layers import AveragePooling2D, MaxPool3D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
 from keras.models import Model
 
-
 def create_discriminator_model():
 
     X_input = Input((16,128,128,3))
@@ -14,26 +13,43 @@ def create_discriminator_model():
     #not sure about the axis in batch norm
     #do we also add dropout after batchnorm/pooling?
 
-    X=Conv3D(filters=32,kernel_size=(2,2,2))(X_input)
-    X=BatchNormalization(axis=-1)(X)
+    #Convolutional Layers
+    #changed the no of filters 
+    X=Conv3D(filters=32,kernel_size=(2,2,2),padding="same")(X_input)
+    X=BatchNormalization(axis=1)(X)
     X=Activation('relu')(X)
     X=MaxPool3D(pool_size=(2,2,2),strides=(2,2,2))(X)
 
-    X=Conv3D(filters=32,kernel_size=(2,2,2))(X)
-    X=BatchNormalization(axis=-1)(X)
+    X=Conv3D(filters=64,kernel_size=(2,2,2),padding="same")(X)
+    X=BatchNormalization(axis=1)(X)
     X=Activation('relu')(X)
     X=MaxPool3D(pool_size=(2,2,2),strides=(2,2,2))(X)
         
-    X=Conv3D(filters=32,kernel_size=(2,2,2))(X)
-    X=BatchNormalization(axis=-1)(X)
+    X=Conv3D(filters=128,kernel_size=(2,2,2),padding="same")(X)
+    X=BatchNormalization(axis=1)(X)
     X=Activation('relu')(X)
     X=MaxPool3D(pool_size=(2,2,2),strides=(2,2,2))(X)
 
+    X=Conv3D(filters=128,kernel_size=(2,2,2),padding="same")(X)
+    X=BatchNormalization(axis=1)(X)
+    X=Activation('relu')(X)
+    X=MaxPool3D(pool_size=(2,2,2),strides=(2,2,2))(X)
+
+    #to add the 5th layer change the cap to 32 frames
+
+    # X=Conv3D(filters=256,kernel_size=(2,2,2),padding="same")(X)
+    # X=BatchNormalization(axis=1)(X)
+    # X=Activation('relu')(X)
+    # X=MaxPool3D(pool_size=(2,2,2),strides=(2,2,2))(X)
+
+
+    #Fully connected layers
+
     X=Flatten()(X)
 
-    X=Dense(128,activation='relu')(X)
     X=Dense(256,activation='relu')(X)
-    X=Dense(512,activation='relu')(X)
+    X=BatchNormalization()(X)
+    #add batch norm to dense layer
     X=Dense(2,activation='softmax')(X)
 
     model=Model(inputs=X_input,outputs=X,name="Dicriminator")
