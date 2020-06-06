7555874e536026e3931fe7196b6205c162c2b59f
Savio <sajo240519@gmail.com>
2020-06-06 18:39:38 +0530

    changed output layer

--- discriminator.py
+++ discriminator.py
@@ -2,6 +2,7 @@ from tensorflow import keras
 from tensorflow.keras.layers import (Input, Dense, Activation,
                                      BatchNormalization, Flatten,
                                      MaxPool3D, Conv3D)
+from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras import Model
 
 
@@ -46,11 +47,13 @@ def create_discriminator_model():
     X = Flatten()(X)
 
     X = Dense(256, activation='relu')(X)
-    X = BatchNormalization()(X)
     # add batch norm to dense layer
-    X = Dense(2, activation='softmax')(X)
+    X = BatchNormalization()(X)
+    # activation done with loss fn
+    # for numerical stability
+    X = Dense(1, activation=None)(X)
 
-    model = Model(inputs=X_input, outputs=X, name="Dicriminator")
+    model = Model(inputs=X_input, outputs=X, name="Discriminator")
 
     return model
 
@@ -58,6 +61,8 @@ def create_discriminator_model():
 if __name__ == "__main__":
     discriminator = create_discriminator_model()
     opt = keras.optimizers.Adam(lr=0.001)
-    discriminator.compile(loss='binary_crossentropy',
-                          optimizer=opt, metrics=['accuracy'])
+    loss = BinaryCrossentropy(from_logits=True)
+    discriminator.compile(loss=loss,
+                          optimizer=opt,
+                          metrics=['accuracy'])
     print(discriminator.summary())
