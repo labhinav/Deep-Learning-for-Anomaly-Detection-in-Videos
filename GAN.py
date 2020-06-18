14d2704ceb115a54ebce0b898a7bbb082a9faa67
goan-coder <abhinavlalwani@gmail.com>
2020-06-18 21:18:43 +0530

    updated GAN

--- GAN.py
+++ GAN.py
@@ -8,14 +8,14 @@ from tensorflow.keras.layers import Input, Dense, Activation, ZeroPadding2D, Bat
 from tensorflow.keras.layers import AveragePooling2D, MaxPool3D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 import tensorflow.keras.backend as K
-
+from tf2_multi_preprocessing import build_dataset
 class GAN():
-    def __init__(self):
+    def __init__(self, mini_batch_size):
         self.image_shape=(16,128,128,3)
         learning_rate=0.03
         opt=keras.optimizers.Adam(lr=learning_rate)
         opt1=keras.optimizers.Adam(lr=learning_rate)
-        opt_slow=keras.optimizers.Adam(lr=10*learning_rate)
+        opt_slow=keras.optimizers.Adam(lr=1)
         #Build and compile the discriminator
         self.discriminator=create_discriminator_model()
         self.discriminator.compile(loss='binary_crossentropy',optimizer=opt,metrics=['accuracy'])
@@ -30,45 +30,48 @@ class GAN():
         validity = self.discriminator(img)
         self.combined = Model(z, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=opt1,metrics=['accuracy'])
+        self.dir_path = '/kaggle/input/ucf-crime-training-subset/tfrecords2/'
+        self.ds = build_dataset(self.dir_path, batch_size=mini_batch_size,file_buffer=512*1024)
 
     def train(self,epochs,mini_batch_size):
         #this function will need to be added later
+        tf.summary.trace_off()
         for epoch in range(epochs):
-            d_loss_sum=np.zeros(2)
+            d_loss_sum=tf.zeros(2)
             reconstruct_error_sum=0
-            g_loss_sum=np.zeros(2)
-            for i in range(68):
+            g_loss_sum=tf.zeros(2)
+            no_of_minibatches=0
+            for minibatch,labels in self.ds:
                 # ---------------------
                 #  Train Discriminator
                 # ---------------------
-                minibatch=np.load('/kaggle/working/shuffled_minibatch%d.npz' %(i))
-                minibatch=minibatch['arr_0']
-                minibatch=K.cast(minibatch,'float32')
                 #normalize inputs
-                minibatch/=255
+                no_of_minibatches+=1
+                minibatch=tf.cast(tf.math.divide(minibatch,255), tf.float32)
                 gen_vids=self.generator.predict(minibatch)
                 #might have to combine these to improve batch norm
-                d_loss_real=self.discriminator.train_on_batch(minibatch,np.ones((mini_batch_size,1)))
-                d_loss_fake=self.discriminator.train_on_batch(gen_vids,np.zeros((mini_batch_size,1)))
-                d_loss=0.5*np.add(d_loss_real,d_loss_fake)
+                d_loss_real=self.discriminator.train_on_batch(minibatch,tf.ones((mini_batch_size,1)))
+                d_loss_fake=self.discriminator.train_on_batch(gen_vids,tf.zeros((mini_batch_size,1)))
+                d_loss=0.5*tf.math.add(d_loss_real,d_loss_fake)
                 # ---------------------
                 #  Train Generator
                 # ---------------------
                 # The generator wants the discriminator to label the generated samples as valid (ones)
-                valid_y = np.array([1] * mini_batch_size)
+                valid_y = tf.ones((mini_batch_size,1))
                 # Train the generator
                 g_loss = self.combined.train_on_batch(minibatch,valid_y)
                 reconstruct_error=self.generator.train_on_batch(minibatch,minibatch)
                 d_loss_sum+=d_loss
                 g_loss_sum+=g_loss
                 reconstruct_error_sum+=reconstruct_error
-            g_loss=g_loss_sum/68
-            d_loss=d_loss_sum/68
-            reconstruct_error=reconstruct_error_sum/68
+            print(no_of_minibatches)
+            self.combined.save_weights('/kaggle/working/weights_epoch%d' %(epoch))
+            g_loss=g_loss_sum/no_of_minibatches
+            d_loss=d_loss_sum/no_of_minibatches
+            reconstruct_error=reconstruct_error_sum/no_of_minibatches
             # Plot the progress
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f, accuracy %.2f%% from which %f is combined loss and %f is reconstruction loss]" % (epoch, d_loss[0], 100*d_loss[1], g_loss[0]+reconstruct_error,g_loss[1],g_loss[0],reconstruct_error))
-        
-
+            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f, accuracy %.2f%% from which %f is combined loss and %f is reconstruction loss]" % (epoch, d_loss[0], 100*d_loss[1], g_loss[0]+reconstruct_error,g_loss[1]*100,g_loss[0],reconstruct_error))
+        tf.summary.trace_on()
 if __name__ == '__main__':
     gan = GAN()
     print(gan.combined.summary())
